19ba156b2f42b82e390b021995dff751d1c65823
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-12 06:08:00 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   random_num = rand(1..6)
   if guess == "exit"
     puts "Goodbye!"
-    break
+    #break
   elsif guess.to_i == random_num
     puts "You guessed the correct number!"
   else
